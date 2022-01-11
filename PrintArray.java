d95925fc74325b90b09bc4469c55192c1fc77d89
artxe <balgoon24@gmail.com>
2022-01-12 02:27:30 +0900

    기지국 설치

---
+++ PrintArray.java
@@ -0,0 +1,171 @@
+public class PrintArray {
+    static final void print(boolean[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ ");
+        while (i < length) {
+            System.out.print((array[i++] ? 'O' : 'X') + ", ");
+        }
+        System.out.println((array[i] ? 'O' : 'X') + " }");
+    }
+
+    static final void print(boolean[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (boolean[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ ");
+                while (i < length) {
+                    System.out.print((a[i++] ? 'O' : 'X') + ", ");
+                }
+                System.out.println((a[i] ? 'O' : 'X') + " }");
+            }
+        }
+        System.out.println("}");
+    }
+
+    static final void print(char[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ ");
+        while (i < length) {
+            System.out.print(array[i++] + ", ");
+        }
+        System.out.println(array[i] + " }");
+    }
+
+    static final void print(char[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (char[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ ");
+                while (i < length) {
+                    System.out.print(a[i++] + ", ");
+                }
+                System.out.println(a[i] + " }");
+            }
+        }
+        System.out.println("}");
+    }
+
+    static final void print(int[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ ");
+        while (i < length) {
+            System.out.print(array[i++] + ", ");
+        }
+        System.out.println(array[i] + " }");
+    }
+
+    static final void print(int[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (int[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else if (a.length == 0) {
+                System.out.println("\t{}");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ ");
+                while (i < length) {
+                    System.out.print(a[i++] + ", ");
+                }
+                System.out.println(a[i] + " }");
+            }
+        }
+        System.out.println("}");
+    }
+
+    static final void print(Integer[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ ");
+        while (i < length) {
+            System.out.print(array[i++] + ", ");
+        }
+        System.out.println(array[i] + " }");
+    }
+
+    static final void print(Integer[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (Integer[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ ");
+                while (i < length) {
+                    System.out.print(a[i++] + ", ");
+                }
+                System.out.println(a[i] + " }");
+            }
+        }
+        System.out.println("}");
+    }
+
+    static final void print(long[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ ");
+        while (i < length) {
+            System.out.print(array[i++] + ", ");
+        }
+        System.out.println(array[i] + " }");
+    }
+
+    static final void print(long[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (long[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ ");
+                while (i < length) {
+                    System.out.print(a[i++] + ", ");
+                }
+                System.out.println(a[i] + " }");
+            }
+        }
+        System.out.println("}");
+    }
+
+    static final void print(String[] array) {
+        int length = array.length - 1, i = 0;
+        System.out.print("{ \"");
+        while (i < length) {
+            System.out.print(array[i++] + "\", \"");
+        }
+        System.out.println(array[i] + "\" }");
+    }
+
+    static final void print(String[][] array) {
+        int length, i;
+        System.out.print("{\n");
+        for (String[] a : array) {
+            if (a == null) {
+                System.out.println("\tnull, ");
+            } else {
+                length = a.length - 1;
+                i = 0;
+                System.out.print("\t{ \"");
+                while (i < length) {
+                    System.out.print(a[i++] + "\", \"");
+                }
+                System.out.println(a[i] + "\" }");
+            }
+        }
+        System.out.println("}");
+    }
+}
--- Solution.java
+++ Solution.java
@@ -1,16 +1,29 @@
 class Solution {
-    public int solution(int n) {
-        int mod = 1234567;
+    public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int prev = 1;
-        int temp;
+        int index = w + 1;
+        int interval = w * 2 + 1;
 
-        /* fibonacci */
-        while (n-- >= 0) {
-            temp = answer;
-            answer = (answer + prev) % mod;
-            prev = temp;
+        for (int i : stations) {
+            while (index < i) {
+                System.out.println(index + " < " + i);
+                index += interval;
+                answer++;
+            }
+            index = i + interval;
+        }
+        index -= w + 1;
+        while (index < n) {
+            System.out.println(index + " < " + n);
+            index += interval;
+            answer++;
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int answer = new Solution().solution(17, new int[] { 9 }, 2);
+        
+        System.out.println(answer);
     }
 }
