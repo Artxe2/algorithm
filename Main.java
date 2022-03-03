a36582200087babacf97b140cb57ce5a534550be
balgoon24 <balgoon24@gmail.com>
2022-03-03 16:55:35 +0900

    ``

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(i + ": " + new Solution().solution(i));
-        }
+        int[] sales = Gson.fromJson(int[].class, "[14, 17, 15, 18, 19, 14, 13, 16, 28, 17]");
+        int[][] links = Gson.fromJson(int[][].class, "[[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]");
+
+        int answer = new Solution().solution(sales, links);
+
+        System.out.println("answer: " + answer + " == 44: " + (answer == 44));
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,25 +1,33 @@
 class Solution {
-    public int solution(int n) {
-        if (n % 2 > 0) {
-            return 0;
-        }
-        if (n == 2) {
-            return 3;
-        }
-        n = n / 2;
-        
-        int mod = 1000000007;
-        long temp;
-        long[] answer = new long[n + 1];
-        long[] reverse = new long[n + 1];
+    public int solution(int[] sales, int[][] links) {
+        int length = 0;
+        int answer = 0;
+        int[] supervisors = new int[sales.length];
+        boolean[] visit = new boolean[sales.length];
 
-        answer[1] = 3;
-        answer[2] = 11;
-        for (int i = 2; i <= n; i++) {
-            temp = reverse[i - 1] + answer[i - 2] * 2;
-            reverse[i] = temp;
-            answer[i] = (answer[i - 1] * 3 + temp + 2) % mod;
+        for (int[] a : links) {
+            supervisors[a[1] - 1] = a[0] - 1;
         }
-        return (int) answer[n];
+        for (int i : supervisors) {
+            if (!visit[i]) {
+                visit[i] = true;
+                length++;
+            }
+        }
+
+        int[] team = new int[length];
+
+        for (int i : supervisors) {
+            if (visit[i]) {
+                visit[i] = false;
+                team[--length] = i;
+            }
+        }
+        PrintArray.print(team);
+        return dfs();
+    }
+
+    int dfs() {
+        return 0;
     }
 }
