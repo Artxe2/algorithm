8fc5adb9c7a8cd15cd5b1e47bef6570e1c51d6da
balgoon24 <balgoon24@gmail.com>
2022-03-23 17:52:12 +0900

    ``

--- Main.java
+++ Main.java
@@ -1,20 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int answer;
+        int[] answer;
 
-        answer = new Solution().solution(
-            new String[] { "go","gone","guild" }
+        answer = new Solution().solution(1, 10);
+        System.out.println("answer: " + Gson.toJson(answer)
+                + " == [0,1,1,2,1,3,1,4,3,5] ? "
+                + Gson.toJson(answer).equals("[0,1,1,2,1,3,1,4,3,5]")
         );
-        System.out.println("answer: " + answer + " == 7 ? " + (answer == 7));
-
-        answer = new Solution().solution(
-            new String[] { "abc","def","ghi","jklm" }
-        );
-        System.out.println("answer: " + answer + " == 4 ? " + (answer == 4));
-
-        answer = new Solution().solution(
-            new String[] { "word","war","warrior","world" }
-        );
-        System.out.println("answer: " + answer + " == 15 ? " + (answer == 15));
+        // answer = new Solution().solution(49, 55);
+        // System.out.println("answer: " + Gson.toJson(answer)
+        //         + " == [9009009,10000000] ? "
+        //         + Gson.toJson(answer).equals("[9009009,10000000]")
+        // );
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,53 +1,25 @@
 class Solution {
-    class Trie {
-        int value;
-        Trie[] child = new Trie[26];
+    public int[] solution(long begin, long end) {
+        int length = (int) (end - begin) + 1;
+        int[] answer = new int[length];
 
-        Trie get(int i) {
-            return child[i];
-        }
-
-        int value() {
-            return value;
-        }
-
-        void put(String s, int length, int index) {
-            this.value++;
-            if (index < length) {
-                int i = s.charAt(index++) - 97;
-                Trie t = child[i];
-
-                if (t == null) {
-                    t = new Trie();
-                    child[i] = t;
-                }
-                t.put(s, length, index);
+        for (int i = 0; i < length; i++) {
+            if (begin + i > 1) {
+                answer[i] = findMod(begin + i);
             }
-        }
-    }
-
-    public int solution(String[] words) {
-        int answer = 0;
-        int length;
-        int index;
-        Trie root = new Trie();
-        Trie trie;
-
-        for (String s : words) {
-            root.put(s, s.length(), 0);
-        }
-        for (String s : words) {
-            length = s.length();
-            index = 0;
-            trie = root;
-            while (index < length) {
-                trie = trie.get(s.charAt(index++) - 97);
-                if (trie.value() == 1) {
-                    break;
-                }
-            }
-            answer += index;
         }
         return answer;
+    }
+
+    int findMod(long n) {
+        int mod = (int) Math.min(10000000, n / 2);
+        int max = (int) Math.min(mod, Math.sqrt(n));
+
+        for (int i = (int) Math.max(2, (n / mod)); i <= max; i++) {
+            if (n % i == 0) {
+                return (int) Math.max(n / i, n % i);
+            }
+        }
+        return 1;
     }
 }
